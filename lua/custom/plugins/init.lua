fca5ba54b15b1c50abe5dd22e711f4a832e1c470
Ethan Brady <git@ethanbrady.xyz>
2024-10-22 12:33:48 -0600

    Setup neoscroll

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -23,5 +23,58 @@ return {
         vim.keymap.set('n', '<space>-', require('oil').toggle_float, { desc = 'Open oil in a floating window' })
       end,
     },
+    {
+      'karb94/neoscroll.nvim',
+
+      config = function()
+        local neoscroll = require 'neoscroll'
+        local keymap = {
+
+          ['<C-u>'] = function()
+            neoscroll.ctrl_u { duration = 100 }
+          end,
+          ['<C-d>'] = function()
+            neoscroll.ctrl_d { duration = 100 }
+          end,
+          ['<PageUp>'] = function()
+            neoscroll.ctrl_u { duration = 100 }
+          end,
+          ['<PageDown>'] = function()
+            neoscroll.ctrl_d { duration = 100 }
+          end,
+          ['<C-b>'] = function()
+            neoscroll.ctrl_b { duration = 200 }
+          end,
+          ['<C-f>'] = function()
+            neoscroll.ctrl_f { duration = 200 }
+          end,
+          ['<S-PageUp>'] = function()
+            neoscroll.ctrl_b { duration = 200 }
+          end,
+          ['<S-PageDown>'] = function()
+            neoscroll.ctrl_f { duration = 200 }
+          end,
+          ['<C-y>'] = function()
+            neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 })
+          end,
+          ['<C-e>'] = function()
+            neoscroll.scroll(0.1, { move_cursor = false, duration = 100 })
+          end,
+          ['zt'] = function()
+            neoscroll.zt { half_win_duration = 250 }
+          end,
+          ['zz'] = function()
+            neoscroll.zz { half_win_duration = 250 }
+          end,
+          ['zb'] = function()
+            neoscroll.zb { half_win_duration = 250 }
+          end,
+        }
+        local modes = { 'n', 'v', 'x' }
+        for key, func in pairs(keymap) do
+          vim.keymap.set(modes, key, func)
+        end
+      end,
+    },
   },
 }

c433b8f91b6bf8f233ebecd669c9a184a532df0e
Ethan Brady <git@ethanbrady.xyz>
2024-10-22 12:05:53 -0600

    Enabled a couple additional kickstart plugins and setup oil

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,26 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    {
+      'stevearc/oil.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      config = function()
+        require('oil').setup {
+          columns = { 'icon' },
+          keymaps = {
+            ['<C-h>'] = false,
+            ['<M-h>'] = 'actions.select_split',
+          },
+          view_options = {
+            show_hidden = true,
+          },
+        }
+
+        vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+        vim.keymap.set('n', '<space>-', require('oil').toggle_float, { desc = 'Open oil in a floating window' })
+      end,
+    },
+  },
+}

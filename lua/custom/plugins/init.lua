a7c236d0d5e13b6c655611decd58fd21f3495d63
Ethan Brady <git@ethanbrady.xyz>
2024-10-27 16:27:29 -0600

    disabled the <C-j> binding for supermaven-nvim because it didn't work with the cmp menu setup

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -104,9 +104,6 @@ return {
         'supermaven-inc/supermaven-nvim',
         config = function()
           require('supermaven-nvim').setup {
-            keymaps = {
-              accept_word = '<C-j>',
-            },
             disable_keymaps = true,
             disable_inline_completion = true,
           }
